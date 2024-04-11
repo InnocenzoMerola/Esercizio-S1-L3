9954edc42d87bc82e32a0cee21e6b44ee691fb50
Innocenzo Merola <146184569+InnocenzoMerola@users.noreply.github.com>
2024-04-11 16:29:13 +0200

    first commit

---
+++ create.php
@@ -0,0 +1,141 @@
+<?php
+
+$host = 'localhost';
+$db = 'ifoa_office';
+$user = 'root';
+$pass = '';
+
+$dsn = "mysql:host=$host;dbname=$db";
+
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+$stmt = $pdo->query('SELECT * FROM users');
+
+
+echo '<pre>' . print_r($_POST, true) . '</pre>';
+
+$name = $_POST['username'] ?? "";
+$surname = $_POST['surname'] ?? "";
+$email = $_POST['email'] ?? "";
+$age = $_POST['age'] ?? "";
+$password = $_POST['password'] ?? "";
+$profession = $_POST['profession'] ?? "";
+
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+
+    $errors = [];
+
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errors['email'] = 'Email non valida';
+    }
+
+    if(strlen($name) < 5 || strlen($name) > 15){
+        $errors['username'] = "Il nome utente deve contenere dalle 5 alle 15 lettere";
+    }
+    
+    
+    if(strlen($password) < 10 || strlen($password) > 17){
+        $errors['password'] = "La password deve contenere dalle 10 alle 17 lettere";
+    }
+
+    if($age === ""){
+        $errors['age'] = "Aggiungere l'età";
+    }
+
+
+
+    if($errors === []){
+        header('Location: /IFOA-BackEnd/Esercizio%20S1-L2/success.php');
+    }
+}
+
+
+$stmt = $pdo->prepare("INSERT INTO users (name, surname, age, email, password, profession) VALUES (:name; :surname, :age, :email, :password, :profession)");
+$stmt->execute([
+    'name' => $name,
+    'surname' => $surname,
+    'age' => $age,
+    'email' => $email,
+    'password' => $password,
+    'profession' => $profession,
+])
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body style="background-color: #ddd594;">
+    
+<div class="container" style="margin-block: 10rem">
+  <div class="row justify-content-center">
+    <div class="col-5">
+
+  
+<form action="" method="post" novalidate>
+    <div class="row row-gap-2">
+  <div class="col-12">
+    <label for="username" class="form-label">Nome</label>
+    <input type="text" value="<?php echo $name;?>" name="username" class="form-control <?= isset($errors['username']) ? 'is-invalid' : ''?>" id="username" >
+    <?= $errors['username'] ?? "" ?>
+  </div>
+
+  <div class="col-12">
+    <label for="surname" class="form-label">Cognome</label>
+    <input type="text" value="<?php echo $surname;?>" name="surname" class="form-control <?= isset($errors['surname']) ? 'is-invalid' : ''?>" id="surname" >
+    <?= $errors['surname'] ?? "" ?>
+  </div>
+  
+  <div class="col-12">
+    <label for="age" class="form-label">Età</label>
+    <input type="number" name="age" value="<?php echo $age;?>" class="form-control <?= isset($errors['age']) ? 'is-invalid' : ''  ?>" id="age" aria-describedby="validationServer03Feedback" >
+    <?= $errors['age'] ?? '' ?>
+  </div>
+
+  <div class="col-12">
+    <label for="email" class="form-label">Email</label>
+      <input type="text" name="email" value="<?php echo $email;?>" class="form-control <?= isset($errors['email']) ? 'is-invalid' : "" ?>" id="email" >
+      <?= $errors['email'] ?? ""?>
+  </div>
+  
+  <div class="col-12">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" name="password"  class="form-control <?= isset($errors['password']) ? 'is-invalid' : "" ?>" id="password" aria-describedby="validationServer03Feedback" >
+    <?= $errors['password'] ?? "" ?>
+  </div>
+  
+  <div class="col-12">
+    <label for="profession" class="form-label">Professione</label>
+    <input type="text" value="<?php echo $profession;?>" name="profession" class="form-control <?= isset($errors['profession']) ? 'is-invalid' : ''?>" id="profession" >
+    <?= $errors['profession'] ?? "" ?>
+  </div>
+  
+  <div class="col-12 mt-3">
+    <button class="btn btn-primary" type="submit">Crea il tuo profilo</button>
+
+    </div>
+</div>
+</form>
+</div>
+  </div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ homepage.php
@@ -0,0 +1,85 @@
+<?php 
+
+$host = 'localhost';
+$db = 'ifoa_office';
+$user = 'root';
+$pass = '';
+
+$dsn = "mysql:host=$host;dbname=$db";
+
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+$stmt = $pdo->query('SELECT * FROM users');
+
+
+echo '<ul>';
+foreach ($stmt as $row)
+{
+    echo "<li>$row[name]</li>";
+}
+echo '</ul>';
+
+// $id = 1;
+// $id = $_GET['id'];
+// $stmt = $pdo->prepare("SELECT * FROM users WHERE id = ?");
+// $stmt->execute([$id]);
+// $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-10">
+            <button>
+                <a href="./create.php">Iscriviti</a>
+            </button>
+            <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Cognome</th>
+                <th scope="col">Email</th>
+                <th scope="col">Professione</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <?php foreach ($stmt as $row) {?>
+        
+                <th scope="row"><?= $row['id'] ?></th>
+                <td><?= $row['name'] ?></td>
+                <td><?= $row['surname'] ?></td>
+                <td><?= $row['age'] ?></td>
+                <td><?= $row['email'] ?></td>
+                <td><?= $row['password'] ?></td>
+                <td><?= $row['profession'] ?></td><?php
+            }?> 
+
+                </tr>
+            </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
